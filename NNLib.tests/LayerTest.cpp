c8ae1f66955a32a4dd050c2ee1099282aa3789f9
Adam Pankuch <adam.pankuch@gmail.com>
2020-12-20 12:30:00 +0100

    Create basis for NNLib tests

---
+++ NNLib.tests/LayerTest.cpp
@@ -0,0 +1,7 @@
+#include <gtest/gtest.h>
+#include "NNLib.hpp"
+
+TEST(LayerTest, Input)
+{
+
+}
--- main.cpp
+++ main.cpp
@@ -1,34 +1,30 @@
 #include <iostream>
 #include "MathLib.hpp"
 #include "DataLoad.hpp"
+#include "NNLib.hpp"
 
 #include <memory>
 
 using namespace mathlib;
 using namespace mathlib::activation;
 using namespace dataload;
-int main()
-{   
-    Matrix mat1(2,3,{1,2,3,4,5,6});
-    Matrix mat2(3,2,{7,8,9,10,11,12});
-    Matrix outcome = mat1 * mat2;
-    std::cout << mat1;
-    std::cout << mat2;
-    std::cout << outcome;
-    Matrix Omat(3,3);
-    for (int i = 0; i < 9; i++){
-        Omat[i] = i;
-    }
-    Matrix Tmat = Omat.T();
-    std::cout << "Original" << std::endl;
-    std::cout << Omat;
-    std::cout << "TRANSPOSED" << std::endl;
-    std::cout << Tmat;
+using namespace nnlib;
 
-    DataLoader dl("../data/fashion_mnist_train_vectors.csv", "../data/fashion_mnist_train_labels.csv");
-    std::vector<PicData> pd = dl.DataLoader::loadAllData(28,28);
-    std::cout << pd[1].getMat() << '\n';
-    std::cout << pd[1].getLabel() << '\n';
+int main()
+{  
+    std::shared_ptr<ILayer> input = std::make_shared<Input>("Input_name", 5, 1);
+    std::cout << input->getName() << std::endl;
+    std::cout << input->getType() << std::endl;
+    std::cout << input->getOutputHeight() << std::endl;
+    std::cout << input->getOutputWidth() << std::endl;
+    std::cout << input->getNeuronOutput() << std::endl;
+
+    Dense dense("Dense_name", LayerType::HiddenLayer, 10, input, std::make_shared<ReLU>());
+    std::cout << dense.getName() << std::endl;
+    std::cout << dense.getType() << std::endl;
+    std::cout << dense.getOutputHeight() << std::endl;
+    std::cout << dense.getOutputWidth() << std::endl;
+    std::cout << dense.getNeuronOutput() << std::endl;
 
     return 0;
 }
