a8f5304f23fbc45a9465f87ee04b412b43a0bbd3
Tomas <kollat13@gmail.com>
2020-12-22 13:04:35 +0100

    removed useless

--- mathlib/activation/LogSoftmax.h
+++ mathlib/activation/LogSoftmax.h
@@ -9,20 +9,6 @@ namespace mathlib::activation
 class LogSoftMax
 {
 public:
-    static Matrix LogSoftMaxWithCrossEntropy(const Matrix& input, const Matrix& label)
-    {
-        float smSum(0.0);
-        for (auto z : input.getVector())
-        {
-            smSum = smSum + std::exp(z);
-        }
-        Matrix output(1, input.getVector().size());
-        for (int i = 0; i < input.getVector().size(); i++ )
-        {
-            output[i] = (float)-input[i] + std::log(smSum);
-        }
-        return output;
-    }
     static Matrix SoftMax(const Matrix& input, const Matrix& label)
     {
         float smSum(0.0);
@@ -44,7 +30,6 @@ public:
         {
             smSum = smSum + std::exp(z);
         }
-        float xenotropy(0.0);
         int i_correct;
         for (int i = 0; i < input.getVector().size(); i++ )
         {
